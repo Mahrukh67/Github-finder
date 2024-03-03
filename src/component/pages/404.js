ca7429592dcd821886aab3e2577567911a65465f
Mahrukh67 <mahrukh2285@gmail.com>
2024-03-03 22:24:27 +0500

    first commit

--- src/component/pages/404.js
+++ src/component/pages/404.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const NotFound = () => {
-  return (
-    <div className='all-center my-3'>
-      <h1>Not Found!! :(</h1>
-      <p className='lead'>The page you are looking for does not exist...</p>
-      <Link to='/' className='btn btn-primary'>
-        Back to Home
-      </Link>
-    </div>
-  );
+  if(NotFound){
+    return (
+      <div className='all-center my-3'>
+        <h1>Click to Home For Search  :(</h1>
+        <p className='lead'>Looking for a Github Repo Click here</p>
+  
+        <Link to='/' className='btn btn-primary'>
+          Well come
+        </Link>
+      </div>
+    );
+  }
+
+  
+  
+
 };
